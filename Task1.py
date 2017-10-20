dfec1e7df32d7a277c3b8dce39b3433cd3d43fbf
Ganesh Sivakumar <ganesh.s@dal.ca>
2017-10-20 14:19:57 -0300

    commit

--- Task1.py
+++ Task1.py
@@ -5,6 +5,7 @@ import string
 def find_biggest(filename):
     """Find the biggest word in the book"""
     d = dict()
+    d2=dict()
     for line in open(filename):
         line = line.replace('-', ' ')
         for word in line.split():
@@ -12,12 +13,11 @@ def find_biggest(filename):
             #word = word.strip().lower()
             #print(word)
             d[word] = d.get(word, 0) + 1
-    for i in range(len(d)):
-        if len(d[i])<len(d[i+1]):
-            length=len(d[i+1])
-        elif len(d[i])>len(d[i+1]):
-            length=len(d[i])
-    return length
+        for i in d:
+            d2[i]= len(i)
+        for i in d2.items():
+            print(d2.values())
+    #print(d2)
 
 def main():
     find_biggest("Book1.txt")
